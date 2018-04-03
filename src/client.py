40e27948169ce7c7e84ca55db8c82de023bf0d4a
Jay Adams <j.adams011@hotmail.com>
2018-04-02 19:45:38 -0700

    finished cleint.py

--- src/client.py
+++ src/client.py
@@ -4,22 +4,22 @@ import socket
 
 avail = socket.getaddrinfo('127.0.0.1', 3000)
 stream_info = [i for i in infos if i[1] == socket.SOCK_STREAM][0]
-socket.getaddrinfo('127.0.0.1', 3000)
-stream_info
 client = socket.socket(*stream_info[:3])
-client
 client.connect(stream_info[_1])
-message = 'This is awesome. I am a client sending data to my server'
+
+
 client.sendall(message.encode('utf8'))
-conn.send?
-conn.send??
-buffer_length = 8
-message_complete = False
 
-while not message_complete:
+
+recv_buffer = 8
+message = 'This is awesome. I am a client sending data to my server'
+while True:
     part = cleint.recv(buffer_length)
-    print(part.decode('utf8'))
-    if len(part) < buffer_length:
+    message += part.decode('utf8')
+    if len(part) < recv_buffer:
         break
-client.close()
 
+print(message)
+
+
+client.close()
