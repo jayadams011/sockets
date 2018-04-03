e7d818e2ed8cace62da66da1cae92e8589b17458
Jay Adams <j.adams011@hotmail.com>
2018-04-02 20:39:39 -0700

    more debugging and adding date time stamps

--- src/client.py
+++ src/client.py
@@ -7,19 +7,20 @@ stream_info = [i for i in avail if i[1] == socket.SOCK_STREAM][0]
 client = socket.socket(*stream_info[:3])
 client.connect(stream_info[-1])
 
+msg = 'This is awesome. I am a client sending data to my server'
 
-client.sendall(message.encode('utf8'))
+client.sendall(msg.encode('utf8'))
 
 
-recv_buffer = 8
-message = 'This is awesome. I am a client sending data to my server'
+buffer_length = 8
+msg = ''
 while True:
-    part = cleint.recv(buffer_length)
-    message += part.decode('utf8')
-    if len(part) < recv_buffer:
+    part = client.recv(buffer_length)
+    msg += part.decode('utf8')
+    if len(part) < buffer_length:
         break
 
-print(message)
+print(msg)
 
 
 client.close()
--- src/server.py
+++ src/server.py
@@ -1,34 +1,32 @@
 import socket
-from datetime import datetime
+from datetime import datetime as dt
 
 PORT = 3000
+print('---Starting sever on port {} at {}---'.format(PORT, dt.now()))
 
-sock = socket.socket(
-    socket.AF_INET,
-    socket.SOCKET_STREAM,
-    socket.IPPROTO_TCP
-)
-address = ('127.0.0.1', 3000)
-sock.bind(address)
-begining
-sock.listen(1)
+with socket.socket(
+        socket.AF_INET,
+        socket.SOCK_STREAM,
+        socket.IPPROTO_TCP) as sock:
 
-conn, addr = sock.accept()
-message_complete = False
+    address = ('127.0.0.1', 3000)
+    sock.bind(address)
+    sock.listen(1)
 
-message_from_client = ''
+    conn, addr = sock.accept()
+    msg_complete = False
 
-while not message_complete:
-    part = conn.recv(1024)
-    message_from_client += part.decode('utf8')
-    if len(part) < 1024:
-        break
+    msg_from_client = ''
 
-year_day = '2018/12/01'
-t = datetime.strptime(year_day, '%Y/%m/%d')
-print('[{}] Echoed: {}'.format(t, msg_from_client))
+    while not msg_complete:
+        part = conn.recv(1024)
+        msg_from_client += part.decode('utf8')
+        if len(part) < 1024:
+            break
 
-conn.sendall(msg_from_client.encode('utf8'))
-end()
-conn.close()
-sock.close()
+    year_day = '2018/12/01'
+    t = dt.strptime(year_day, '%Y/%m/%d')
+    print('[{}] Echoed: {}'.format(t, msg_from_client))
+
+    print('---Stopping sever on port {} at {}---'.format(PORT, dt.now()))
+    conn.sendall(msg_from_client.encode('utf8'))
