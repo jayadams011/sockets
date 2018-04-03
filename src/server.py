fb35e6738111182bc22c881ff5a09b459490a8fb
Jay Adams <j.adams011@hotmail.com>
2018-04-02 20:18:01 -0700

    got some hlep debugging

--- src/server.py
+++ src/server.py
@@ -8,20 +8,27 @@ sock = socket.socket(
     socket.SOCKET_STREAM,
     socket.IPPROTO_TCP
 )
-sock.bind(('127.0.0.1', 3000))
-sock.listen()
-recv_buffer = 8
+address = ('127.0.0.1', 3000)
+sock.bind(address)
+begining
+sock.listen(1)
 
-while True:
-    try:
-        conn, addr = sock.accept()
-        message = 'We have lift off'
+conn, addr = sock.accept()
+message_complete = False
 
-        while True:
-            part = conn.recv(recv_buffer)
-            message = '[{}] Echoed: 'format(dt.now()) + message
-            if len(part) < recv_buffer:
-                break
-      
+message_from_client = ''
+
+while not message_complete:
+    part = conn.recv(1024)
+    message_from_client += part.decode('utf8')
+    if len(part) < 1024:
+        break
+
+year_day = '2018/12/01'
+t = datetime.strptime(year_day, '%Y/%m/%d')
+print('[{}] Echoed: {}'.format(t, msg_from_client))
+
+conn.sendall(msg_from_client.encode('utf8'))
+end()
 conn.close()
 sock.close()
