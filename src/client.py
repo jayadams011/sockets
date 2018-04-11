da5bbd843e7190b36825d1ccd989ebd63af4fb9d
Jay Adams <j.adams011@hotmail.com>
2018-04-11 13:25:09 -0700

    fix client and server code

--- src/client.py
+++ src/client.py
@@ -2,25 +2,29 @@ import sys
 import socket
 
 
-avail = socket.getaddrinfo('127.0.0.1', 3000)
-stream_info = [i for i in avail if i[1] == socket.SOCK_STREAM][0]
-client = socket.socket(*stream_info[:3])
-client.connect(stream_info[-1])
+def run_client():
+    """run client function """
+    avail = socket.getaddrinfo('127.0.0.1', 3000)
+    stream_info = [i for i in avail if i[1] == socket.SOCK_STREAM][0]
+    client = socket.socket(*stream_info[:3])
+    client.connect(stream_info[-1])
 
-msg = 'This is awesome. I am a client sending data to my server'
+    message = sys.argv[1:]
+    message_to_server = ' '.join(message) 
+    client.sendall(message_to_server.encode('utf8'))
 
-client.sendall(msg.encode('utf8'))
+    buffer_length = 8
+    msg = ''
+    while True:
+        part = client.recv(buffer_length)
+        msg += part.decode('utf8')
+        if len(part) < buffer_length:
+            break
+
+    print(message)
+
+    client.close()
 
 
-buffer_length = 8
-msg = ''
-while True:
-    part = client.recv(buffer_length)
-    msg += part.decode('utf8')
-    if len(part) < buffer_length:
-        break
-
-print(msg)
-
-
-client.close()
+if __name__ == "__main__":
+    run_client()
--- src/server.py
+++ src/server.py
@@ -4,33 +4,41 @@ from datetime import datetime as dt
 PORT = 3000
 print('---Starting sever on port {} at {}---'.format(PORT, dt.now()))
 
-with socket.socket(
-        socket.AF_INET,
-        socket.SOCK_STREAM,
-        socket.IPPROTO_TCP) as sock:
 
-    address = ('127.0.0.1', 3000)
-    sock.bind(address)
-    sock.listen(1)
+def run_server():
+    """ server func """
+    try:
+        with socket.socket(
+                socket.AF_INET,
+                socket.SOCK_STREAM,
+                socket.IPPROTO_TCP) as sock:
 
-    conn, addr = sock.accept()
-    msg_complete = False
+            address = ('127.0.0.1', 3000)
+            sock.bind(address)
+            sock.listen(1)
 
-    msg_from_client = ''
+            conn, addr = sock.accept()
+            msg_complete = False
 
-    while not msg_complete:
-        part = conn.recv(1024)
-        msg_from_client += part.decode('utf8')
-        if len(part) < 1024:
-            break
+            msg_from_client = ''
 
-    year_day = '2018/12/01'
-    t = dt.strptime(year_day, '%Y/%m/%d')
-    print('[{}] Echoed: {}'.format(t, msg_from_client))
+            while not msg_complete:
+                part = conn.recv(1024)
+                msg_from_client += part.decode('utf8')
+                if len(part) < 1024:
+                    break
 
-    print('---Stopping sever on port {} at {}---'.format(PORT, dt.now()))
-    conn.sendall(msg_from_client.encode('utf8'))
+            year_day = '2018/12/01'
+            t = dt.strptime(year_day, '%Y/%m/%d')
+            print('[{}] Echoed: {}'.format(t, msg_from_client))
 
-except KeyboardInterrupt:
-        print('Stopping server')
-        exit()
+            print('---Stopping sever on port {} at {}---'.format(PORT, 
+                  dt.now()))
+            conn.sendall(msg_from_client.encode('utf8'))
+
+    except KeyboardInterrupt:
+            print('Stopping server')
+            exit()
+
+if __name__ == "__main__":
+    run_server()
